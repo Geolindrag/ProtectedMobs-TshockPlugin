158b62c1a4ae924ea49ebbd25b7f0fc4ffdc1768
Geo <104006156+Geolindrag@users.noreply.github.com>
2023-05-01 16:46:31 -0600

    Add files via upload

---
+++ Protected Mobs v1/ProtectedMobs/Config.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace ProtectedMobs
+{
+    public class Config
+    {
+        public int[] ProtectedMob = { -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        public string KilledMessage = "You were sniped for killing a protected mob!";
+        public byte Red = 255;
+        public byte Blue = 255;
+        public byte Green = 255;
+
+        public void Write(){
+            File.WriteAllText(ProtectedMobs.path, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+
+        public static Config Read(){
+            if (!File.Exists(ProtectedMobs.path))
+            return new Config();
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(ProtectedMobs.path));
+        }
+    }
+}
---
+++ Protected Mobs v1/ProtectedMobs/ProtectedMobs.cs
@@ -0,0 +1,56 @@
+﻿using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
+using Microsoft.Xna.Framework;
+
+namespace ProtectedMobs
+{
+    [ApiVersion(2, 1)]
+    public class ProtectedMobs : TerrariaPlugin
+    {
+        public override string Name => "Protected mobs";
+        public override Version Version => new Version(1, 0, 0);
+        public override string Author => "Geolindrag";
+        public override string Description => "Kills the closest player if a specific mobs is killed by anything";
+        //Json Reading stuff
+        public static string path = Path.Combine(TShock.SavePath + "/ProtectedMobs.json");
+        public static Config Config = new Config();
+        //
+        public ProtectedMobs(Main game) : base(game){}
+
+        public override void Initialize()
+        {
+            GeneralHooks.ReloadEvent += OnReload;
+            ServerApi.Hooks.NpcKilled.Register(this, OnKill);
+            if (File.Exists(path))
+                Config = Config.Read();
+            else
+                Config.Write();
+        }
+
+        private void OnKill(NpcKilledEventArgs args)
+        {
+            TSPlayer player = TShock.Players[args.npc.FindClosestPlayer()];
+            for (int i = 0; i < Config.ProtectedMob.Length; i++)
+            {
+                if (args.npc.netID == Config.ProtectedMob[i])
+                {
+                    if (!player.Dead)
+                    {
+                        player.KillPlayer();
+                        player.SendMessage(Config.KilledMessage, new Color { R = Config.Red, B = Config.Blue, G = Config.Green });
+                    }
+                        break;   
+                }
+            }
+        }
+        private void OnReload(ReloadEventArgs e)
+        {
+            if (File.Exists(path))
+                Config = Config.Read();
+            else
+                Config.Write();
+        }
+    }
+}
