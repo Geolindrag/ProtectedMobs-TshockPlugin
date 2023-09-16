2e4ceb3a7e4e6d0ec5174320b886b30a1e4172be
Geo <104006156+Geolindrag@users.noreply.github.com>
2023-09-16 11:14:21 -0600

    Minor update to Config
    Added a better example for the death message

--- Protected Mobs v1/ProtectedMobs/Config.cs
+++ Protected Mobs v1/ProtectedMobs/Config.cs
@@ -1,22 +1,24 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace ProtectedMobs
 {
     public class Config
     {
         public int[] ProtectedMob = { -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        public string KilledMessage = "You were sniped for killing a protected mob!";
+        public string KilledMessage = "{1} Were killed for murdering a {0}";
         public byte Red = 255;
         public byte Blue = 255;
         public byte Green = 255;
 
-        public void Write(){
+        public void Write()
+        {
             File.WriteAllText(ProtectedMobs.path, JsonConvert.SerializeObject(this, Formatting.Indented));
         }
 
-        public static Config Read(){
+        public static Config Read()
+        {
             if (!File.Exists(ProtectedMobs.path))
-            return new Config();
+                return new Config();
             return JsonConvert.DeserializeObject<Config>(File.ReadAllText(ProtectedMobs.path));
         }
     }
