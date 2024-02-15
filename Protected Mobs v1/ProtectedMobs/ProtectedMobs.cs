3305ce268bd48d60296b05986a3ca4ece8a1c7e1
Geo <104006156+Geolindrag@users.noreply.github.com>
2024-02-15 17:55:05 -0600

    Added Functions
    Now has 3 different "Modes" of working

--- Protected Mobs v1/ProtectedMobs/ProtectedMobs.cs
+++ Protected Mobs v1/ProtectedMobs/ProtectedMobs.cs
@@ -1,8 +1,10 @@
 using Microsoft.Xna.Framework;
+using On.Terraria;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 using TShockAPI.Hooks;
+using Main = Terraria.Main;
 
 namespace ProtectedMobs
 {
@@ -10,7 +12,7 @@ namespace ProtectedMobs
     public class ProtectedMobs : TerrariaPlugin
     {
         public override string Name => "Protected mobs";
-        public override Version Version => new Version(1, 1, 6);
+        public override Version Version => new Version(1, 3, 0);
         public override string Author => "Geolindrag";
         public override string Description => "Kills the closest player if a specific mobs is killed by anything";
         //Json Reading stuff
@@ -22,6 +24,7 @@ namespace ProtectedMobs
         public override void Initialize()
         {
             GeneralHooks.ReloadEvent += OnReload;
+            //Commands.ChatCommands.Add(new Command(Permissions.canchat,))
             //ServerApi.Hooks.NpcKilled.Register(this, OnKill);
             ServerApi.Hooks.NpcStrike.Register(this, OnDamage);
             if (File.Exists(path))
@@ -40,25 +43,42 @@ namespace ProtectedMobs
                 {
                     if (args.Npc.netID == Config.ProtectedMob[i])
                     {
-                        if (!player.Dead)
+                        switch (Config.Mode)
                         {
-                            string Message = Config.KilledMessage;
-                            player.KillPlayer();
-                            if (Config.KilledMessage.Contains("{0}"))
-                            {
-                                Message = Message.Replace("{0}", args.Npc.FullName.ToString());
-                            }
-                            if (Config.KilledMessage.Contains("{1}"))
-                            {
-                                Message = Message.Replace("{1}", player.Name.ToString());
-                            }
-                            player.SendMessage(Message, new Color { R = Config.Red, B = Config.Blue, G = Config.Green });
-
+                            case 0://mode 0,Just Kill
+                                if (!player.Dead)
+                                {
+                                    string Message = Config.KilledMessage;
+                                    player.KillPlayer();
+                                    if (Config.KilledMessage.Contains("{0}"))
+                                    {
+                                        Message = Message.Replace("{0}", args.Npc.FullName.ToString());
+                                    }
+                                    if (Config.KilledMessage.Contains("{1}"))
+                                    {
+                                        Message = Message.Replace("{1}", player.Name.ToString());
+                                    }
+                                    player.SendMessage(Message, new Color { R = Config.Red, B = Config.Blue, G = Config.Green });
+                                }
+                                break;
+                            case 1: //Mode 1 , summon Boss/Mob
+                                for (int limit = 0; limit < Config.Amt; limit++)
+                                {
+                                    Terraria.NPC.SpawnBoss(((int)player.X) + (32 * (limit + 1)), ((int)player.Y) + 200, Config.SummonMob, player.Index);
+                                }
+                                break;
+                            case 2: //Mode 2 Create projectile, Intended for explotions mostly
+                                Terraria.Projectile.NewProjectile(Terraria.Projectile.GetNoneSource(), args.Npc.VisualPosition.X, args.Npc.VisualPosition.Y, 0, 0, Config.Mode2Proj, 500, 0.0f, -1, 0, 0, 0);
+                                break;   
+                            default:
+                                player.SendMessage("Not valid Mode", new Color { R = 255, B = 50, G = 50 });
+                                break;
                         }
                         break;
                     }
                 }
             }
+
         }
         //Legacy Stuff that may be useful, or not
         /* private void OnKill(NpcKilledEventArgs args)
